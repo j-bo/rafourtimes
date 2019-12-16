c2a054010b82beccd5e2699eb6b1daa575be0f14
j-bo <j.boucheteil@gmail.com>
2019-12-16 18:35:14 +0100

    Removed useless subscriptions

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,6 @@ import { TimeStatusBar } from "./timestatusbar";
 let time_sb = new TimeStatusBar();
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(time_sb.statusBarItem);
-	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(time_sb.update));
-	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(time_sb.update));
 	time_sb.update();
 }
 
